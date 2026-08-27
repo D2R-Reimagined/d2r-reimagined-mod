2e60c863ad31fc698513cc8151e720128904551c
Collin Herber <rustaddicts@gmail.com>
2026-08-27 10:42:31 -0500

    wip: add in rogue exile sorcs

--- scripts/copy-files.bat
+++ scripts/copy-files.bat
@@ -2,4 +2,4 @@
 echo Deleting old files
 rm -R "C:\Program Files (x86)\Diablo II Resurrected\mods\Reimagined\Reimagined.mpq\data"
 echo Copying files
-robocopy "C:\dev\d2r\d2r-reimagined\data" "C:\Program Files (x86)\Diablo II Resurrected\mods\Reimagined\Reimagined.mpq\data" /MIR /E /IS /IT
+robocopy "C:\dev\d2r\d2r-reimagined-mod\data" "C:\Program Files (x86)\Diablo II Resurrected\mods\Reimagined\Reimagined.mpq\data" /MIR /E /IS /IT
